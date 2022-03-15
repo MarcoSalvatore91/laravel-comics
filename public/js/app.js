582a9c923d97c9ab830ccfe52a3cbb9c5d4e17ee
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-03-15 17:08:22 +0100

    header

--- public/js/app.js
+++ public/js/app.js
@@ -19414,26 +19414,26 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
-/***/ "./resources/sass/app.scss":
-/*!*********************************!*\
-  !*** ./resources/sass/app.scss ***!
-  \*********************************/
+/***/ "./resources/sass/style.scss":
+/*!***********************************!*\
+  !*** ./resources/sass/style.scss ***!
+  \***********************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open 'C:\\Laravel\\laravel-comics\\resources\\sass\\app.scss'\n    at C:\\Laravel\\laravel-comics\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\Laravel\\laravel-comics\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Laravel\\laravel-comics\\node_modules\\loader-runner\\lib\\LoaderRunner.js:203:19\n    at C:\\Laravel\\laravel-comics\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (node:internal/process/task_queues:78:11)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!***************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/style.scss ***!
+  \***************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\Laravel\laravel-comics\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Laravel\laravel-comics\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\Laravel\laravel-comics\resources\sass\style.scss */"./resources/sass/style.scss");
 
 
 /***/ })
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
-    <p>Ciao</p>
+    <header>
+
+        <div class="section-top-header">
+            <div class="container">
+                <div class="info-top-header">
+                    <span>DC POWER VISA</span>
+                    <span>ADDITIONAL DC SITES</span>
+                </div>
+            </div>
+        </div>
+
+        <!-- NavBar -->
+
+        <div class="container">
+            <div class="nav-bar">
+                <img src="{{ asset('images/dc-logo.png') }}" alt="{{ asset('images/dc-logo.png') }}">
+            </div>
+        </div>
+
+    </header>
 </body>
 
 </html>
