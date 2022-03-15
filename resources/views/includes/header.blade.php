44878456f3dc3bbc8fb3807c51055a8149063af5
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-03-15 17:23:28 +0100

    created includes and main

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+
+    <div class="section-top-header">
+        <div class="container">
+            <div class="info-top-header">
+                <span>DC POWER VISA</span>
+                <span>ADDITIONAL DC SITES</span>
+            </div>
+        </div>
+    </div>
+
+    <!-- NavBar -->
+
+    <div class="container">
+        <div class="nav-bar">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="{{ asset('images/dc-logo.png') }}">
+        </div>
+    </div>
+
+</header>
