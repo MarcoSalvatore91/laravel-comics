2696e221dc23d89d4e1101e495e647d1e984215c
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-03-15 16:22:10 +0100

    added scss

--- public/js/app.js
+++ public/js/app.js
@@ -19421,7 +19421,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open 'C:\\Laravel\\laravel-comics\\resources\\sass\\app.scss'\n    at C:\\Laravel\\laravel-comics\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\Laravel\\laravel-comics\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Laravel\\laravel-comics\\node_modules\\loader-runner\\lib\\LoaderRunner.js:203:19\n    at C:\\Laravel\\laravel-comics\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (node:internal/process/task_queues:78:11)");
 
 /***/ }),
 
